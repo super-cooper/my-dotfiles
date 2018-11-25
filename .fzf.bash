fb89052d9f12e2d4720641d53dd9b32dd7625bbb
super-cooper <adamcooperrc@gmail.com>
2018-11-24 22:12:22 -0500

    add a bunch of dotfiles

---
+++ .fzf.bash
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/adam/.fzf/bin* ]]; then
+  export PATH="$PATH:/home/adam/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/adam/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/adam/.fzf/shell/key-bindings.bash"
+
