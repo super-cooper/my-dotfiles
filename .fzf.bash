a8bffddd64d385a8a6a0364c28297959ca51d070
Adam Cooper <adamcooperrc@gmail.com>
2019-08-05 11:01:41 -0400

    Minor work computer changes

--- .fzf.bash
+++ .fzf.bash
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/adam/.fzf/bin* ]]; then
-  export PATH="$PATH:/home/adam/.fzf/bin"
+  export PATH="${PATH:+${PATH}:}/home/adam/.fzf/bin"
 fi
 
 # Auto-completion
@@ -11,4 +11,3 @@ fi
 # Key bindings
 # ------------
 source "/home/adam/.fzf/shell/key-bindings.bash"
-
